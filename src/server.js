56ef47f6ebd1b35b1838cbe244a2e3f51fcf096e
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-08-12 00:56:52 -0700

    update

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+const initRoutes = require("./routes/");
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+initRoutes(app);
+
+app.listen(port, ()=> {
+    console.log(`API is running in ${port}`);
+});
